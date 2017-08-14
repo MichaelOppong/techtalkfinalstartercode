607f3359a5b034fffe64da5e533b1c36e8047e95
Michael Oppong <michael@scripted.org>
2017-08-14 22:44:58 +0000

    blah

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // JavaScript File
 // JavaScript File
 $( document ).ready(function() {
-
+$("button").click(function(){
+    $("div").append('<img src="https://i.imgur.com/rNGrgKQ.jpg">');
+        $("div").append('<img src="https://i.imgur.com/ehs4TYd.jpg">');
+});
 
 });
